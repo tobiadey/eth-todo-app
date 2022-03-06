e622c0590973e298df0536b039b36efdbadcc08c
Tobi Adewunmi <OLUWATOBI.ADEWUNMI@CITY.AC.UK>
2022-03-06 09:03:49 +0000

    v1

---
+++ contracts/TodoList.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+
+  // state variable to keep track of the number of tasks inside the todo list.
+  uint public taskCount = 0;
+  //allows to use id as key to find tasks (basically search task by id)
+  mapping(uint => Task) public tasks;
+
+  //definition of a data type of Task using structs
+  struct Task {
+    //attributes of the struct
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  constructor() public {
+    createTask("Create a second task!");
+  }
+
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
+  //create tasks
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
+}
